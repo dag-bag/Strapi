6616b7a987637e69212761b7d43a416b8bdc7a64
dag-bag <virenderkumar23435@gmail.com>
2022-07-06 20:19:11 +0530

    main

--- config/database.js
+++ config/database.js
@@ -1,15 +1,17 @@
+console.log(process.env.username);
 module.exports = ({ env }) => ({
   connection: {
     client: "mysql",
     connection: {
-      host: env("host"),
-      database: env("database"),
-      user: env("username"),
-      password: env("password"),
+      host: process.env.hostF,
+      database: process.env.database,
+      user: process.env.username_h,
+      password: process.env.password,
       ssl: env.bool("DATABASE_SSL", true),
     },
   },
 });
+
 // host: env("DATABASE_HOST", "sql610.main-hosting.eu"),
 //       database: env("DATABASE_NAME", "u779264867_demo"),
 //       user: env("DATABASE_USERNAME", "u779264867_demo"),
