44093794540696591f0cbb0442aa9a257c561165
dag-bag <virenderkumar23435@gmail.com>
2022-07-06 22:23:08 +0530

    main

--- config/database.js
+++ config/database.js
@@ -10,9 +10,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
-
-// host: env("DATABASE_HOST", "sql610.main-hosting.eu"),
-//       database: env("DATABASE_NAME", "u779264867_demo"),
-//       user: env("DATABASE_USERNAME", "u779264867_demo"),
-//       password: env("DATABASE_PASSWORD", "demo@1Dddd"),
-//       ssl: env.bool("DATABASE_SSL", true),
