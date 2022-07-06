8ef92b8575dd438636c99102786ce5a896c37821
dag-bag <virenderkumar23435@gmail.com>
2022-07-06 20:36:20 +0530

    main

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: process.env.APP_KEYS.split(", "),
+    //  process.env.array("APP_KEYS"),
   },
 });
